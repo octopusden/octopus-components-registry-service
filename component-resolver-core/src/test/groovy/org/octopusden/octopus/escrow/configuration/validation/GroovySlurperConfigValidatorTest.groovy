79f98f7584346a725e4826b39b7dec854654934e
Philip Gorbachev <phil.gorbachev@gmail.com>
2023-09-05 16:22:34 +0300

    Add outright prohibited symbols (by Artifactory rules) (#14)

--- component-resolver-core/src/test/groovy/org/octopusden/octopus/escrow/configuration/validation/GroovySlurperConfigValidatorTest.groovy
+++ component-resolver-core/src/test/groovy/org/octopusden/octopus/escrow/configuration/validation/GroovySlurperConfigValidatorTest.groovy
@@ -1,10 +1,8 @@
 package org.octopusden.octopus.escrow.configuration.validation
 
-
-import static org.octopusden.octopus.escrow.configuration.validation.GroovySlurperConfigValidator.GAV_PATTERN
 import static org.octopusden.octopus.escrow.configuration.validation.GroovySlurperConfigValidator.DEB_PATTERN
+import static org.octopusden.octopus.escrow.configuration.validation.GroovySlurperConfigValidator.GAV_PATTERN
 import static org.octopusden.octopus.escrow.configuration.validation.GroovySlurperConfigValidator.RPM_PATTERN
-
 
 class GroovySlurperConfigValidatorTest extends GroovyTestCase {
 
@@ -17,15 +15,15 @@ class GroovySlurperConfigValidatorTest extends GroovyTestCase {
 
     void testDEBPattern() {
         assert DEB_PATTERN.matcher("foundationdb-clients_1.0.amd64.deb").matches()
-        assert DEB_PATTERN.matcher("logcomp_1.0.54-1_amd64.deb,mesh-agent2_2.0.101-1_amd64.deb").matches()
+        assert DEB_PATTERN.matcher("pool/l/logcomp/logcomp_1.0.54-1_amd64.deb,mesh-agent2_2.0.101-1_amd64.deb").matches()
         assert !DEB_PATTERN.matcher("logcomp_1.0.54-1_amd64.deb,file:///dir/file").matches()
         assert !DEB_PATTERN.matcher("foundationdb clients_1.0.amd64.deb").matches()
     }
 
     void testRPMPattern() {
         assert RPM_PATTERN.matcher("ansible-2.11.6-7.el8.noarch.rpm").matches()
-        assert RPM_PATTERN.matcher("ansible-2.11.6-7.el8.noarch.rpm,ansible-core-2.11.6-7.el8.noarch.rpm").matches()
+        assert RPM_PATTERN.matcher("ansible/ansible-2.11.6-7.el8.noarch.rpm,ansible-core-2.11.6-7.el8.noarch.rpm").matches()
         assert !RPM_PATTERN.matcher("ansible-2.11.6-7.el8.noarch.rpm,file:///dir/file").matches()
-        assert !RPM_PATTERN.matcher("ansible-2.11.6/7.el8.noarch.rpm").matches()
+        assert !RPM_PATTERN.matcher("ansible-2.11.6+7.el8.noarch.rpm").matches()
     }
 }
