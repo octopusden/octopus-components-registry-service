d405f8e5029da8903df3953efe7bbf68f6f00733
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-11-22 18:55:45 +0300

    Skip archived components in version prefix intersection check

--- component-resolver-core/src/main/groovy/org.octopusden/octopus/escrow/configuration/validation/EscrowConfigValidator.groovy
+++ component-resolver-core/src/main/groovy/org.octopusden/octopus/escrow/configuration/validation/EscrowConfigValidator.groovy
@@ -25,6 +25,7 @@ import org.octopusden.releng.versions.VersionRangeFactory
 
 @TypeChecked
 class EscrowConfigValidator {
+    static final String ARCHIVED_SUFFIX = "(archived)"
 
     static VersionRangeHelper versionRangeHelper = new VersionRangeHelper()
     static MavenArtifactMatcher mavenArtifactMatcher = new MavenArtifactMatcher()
@@ -178,10 +179,12 @@ class EscrowConfigValidator {
         def jiraProjectKeyAndVersionPrefixToComponentNames = new HashMap<Tuple2<String, String>, HashSet<String>>()
         configuration.escrowModules.each { componentName, escrowModule ->
             escrowModule.moduleConfigurations.each { moduleConfiguration ->
-                jiraProjectKeyAndVersionPrefixToComponentNames.computeIfAbsent(new Tuple2<>(
-                        moduleConfiguration.jiraConfiguration.projectKey,
-                        moduleConfiguration.jiraConfiguration.componentInfo?.versionPrefix
-                )) { new HashSet<>() }.add(componentName)
+                if (!moduleConfiguration.componentDisplayName?.endsWith(ARCHIVED_SUFFIX)) {
+                    jiraProjectKeyAndVersionPrefixToComponentNames.computeIfAbsent(new Tuple2<>(
+                            moduleConfiguration.jiraConfiguration.projectKey,
+                            moduleConfiguration.jiraConfiguration.componentInfo?.versionPrefix
+                    )) { new HashSet<>() }.add(componentName)
+                }
             }
         }
         jiraProjectKeyAndVersionPrefixToComponentNames.each { jiraProjectKeyAndVersionPrefix, componentNames ->
@@ -224,7 +227,7 @@ class EscrowConfigValidator {
         configuration.escrowModules.each { componentKey, value ->
             value.moduleConfigurations.each { config ->
                 //TODO Use appropriate attribute to check if component is archived
-                if (config?.componentDisplayName?.endsWith("(archived)")) {
+                if (config?.componentDisplayName?.endsWith(ARCHIVED_SUFFIX)) {
                     if (config.distribution.explicit() && config.distribution.external()) {
                         registerError("Archived component '$componentKey' can't be explicitly distributed. Pls set distribution->explicit=false")
                     }
