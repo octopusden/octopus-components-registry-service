8a82faa2b2413111f1b73a09f6e684c42c825918
Alexey Zverev <ozonophore@gmail.com>
2025-04-09 11:50:53 +0300

    Fix the handling of the nullable field hotfixVersionFormat

--- components-registry-service-core/src/main/kotlin/org/octopusden/octopus/components/registry/core/dto/ComponentRegistryVersion.kt
+++ components-registry-service-core/src/main/kotlin/org/octopusden/octopus/components/registry/core/dto/ComponentRegistryVersion.kt
@@ -1,11 +1,13 @@
 package org.octopusden.octopus.components.registry.core.dto
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ComponentRegistryVersion(
     @JsonProperty("type") val type: ComponentVersionType,
     @JsonProperty("version") val version: String,
-    @JsonProperty("jiraVersion") val jiraVersion: String
+    @JsonProperty("jiraVersion") val jiraVersion: String?
 )
