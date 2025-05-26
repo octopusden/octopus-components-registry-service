0c5bf73b55608f63c61ba2529ec3357eaeda75bb
adellinekania <adellinekaniasetiyawan@gmail.com>
2025-05-26 10:36:27 +0700

    change to extension functions on VCSSettingsDTO

--- components-registry-service-core/src/main/kotlin/org/octopusden/octopus/components/registry/core/dto/VCSSettingsDTO.kt
+++ components-registry-service-core/src/main/kotlin/org/octopusden/octopus/components/registry/core/dto/VCSSettingsDTO.kt
@@ -9,17 +9,18 @@ import org.octopusden.octopus.components.registry.core.exceptions.VCSConfigurati
 data class VCSSettingsDTO @JsonCreator constructor(
     @JsonProperty("versionControlSystemRoots") val versionControlSystemRoots: List<VersionControlSystemRootDTO> = emptyList(),
     @JsonProperty("externalRegistry") val externalRegistry: String? = null,
-) {
-    fun hasNoConfiguredVCSRoot(): Boolean {
-        return versionControlSystemRoots.isEmpty() || versionControlSystemRoots.size == 1 && versionControlSystemRoots[0].vcsPath.isNullOrEmpty()
+)
+
+fun VCSSettingsDTO.hasNoConfiguredVCSRoot(): Boolean {
+    return versionControlSystemRoots.isEmpty() || versionControlSystemRoots.size == 1 && versionControlSystemRoots[0].vcsPath.isNullOrEmpty()
+}
+
+fun VCSSettingsDTO.getSingleVCSRoot(): VersionControlSystemRootDTO {
+    if (versionControlSystemRoots.isEmpty()) {
+        throw VCSConfigurationException("No VCS Roots are defined in the component")
     }
-    fun getSingleVCSRoot(): VersionControlSystemRootDTO {
-        if (versionControlSystemRoots.isEmpty()) {
-            throw VCSConfigurationException("No VCS Roots are defined in the component")
-        }
-        if (versionControlSystemRoots.size != 1) {
-            throw VCSConfigurationException("Several VCS Roots $versionControlSystemRoots are not supported for the component")
-        }
-        return versionControlSystemRoots[0]
+    if (versionControlSystemRoots.size != 1) {
+        throw VCSConfigurationException("Several VCS Roots $versionControlSystemRoots are not supported for the component")
     }
+    return versionControlSystemRoots[0]
 }
