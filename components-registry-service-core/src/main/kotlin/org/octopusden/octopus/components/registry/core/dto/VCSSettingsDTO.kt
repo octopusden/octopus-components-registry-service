0bf62550189fc77681445a6e594cf1116cd76878
adellinekania <adellinekaniasetiyawan@gmail.com>
2025-06-23 13:31:20 +0700

    remove function from VCSSettingsDTO

--- components-registry-service-core/src/main/kotlin/org/octopusden/octopus/components/registry/core/dto/VCSSettingsDTO.kt
+++ components-registry-service-core/src/main/kotlin/org/octopusden/octopus/components/registry/core/dto/VCSSettingsDTO.kt
@@ -3,24 +3,9 @@ package org.octopusden.octopus.components.registry.core.dto
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.octopusden.octopus.components.registry.core.exceptions.VCSConfigurationException
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class VCSSettingsDTO @JsonCreator constructor(
     @JsonProperty("versionControlSystemRoots") val versionControlSystemRoots: List<VersionControlSystemRootDTO> = emptyList(),
     @JsonProperty("externalRegistry") val externalRegistry: String? = null,
 )
-
-fun VCSSettingsDTO.hasNoConfiguredVCSRoot(): Boolean {
-    return versionControlSystemRoots.isEmpty() || versionControlSystemRoots.size == 1 && versionControlSystemRoots[0].vcsPath.isNullOrEmpty()
-}
-
-fun VCSSettingsDTO.getSingleVCSRoot(): VersionControlSystemRootDTO {
-    if (versionControlSystemRoots.isEmpty()) {
-        throw VCSConfigurationException("No VCS Roots are defined in the component")
-    }
-    if (versionControlSystemRoots.size != 1) {
-        throw VCSConfigurationException("Several VCS Roots $versionControlSystemRoots are not supported for the component")
-    }
-    return versionControlSystemRoots[0]
-}
