2cf81715f813b517ee0549cfc718b715d3dd5dfb
Mark Rode <rode.mark01@gmail.com>
2025-11-01 12:11:14 +0300

    add support list of release managers

--- component-resolver-core/src/test/java/org/octopusden/octopus/escrow/configuration/validation/ComponentsRegistryValidatorTest.java
+++ component-resolver-core/src/test/java/org/octopusden/octopus/escrow/configuration/validation/ComponentsRegistryValidatorTest.java
@@ -4,7 +4,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Objects;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +15,8 @@ import org.octopusden.octopus.escrow.configuration.loader.ConfigLoader;
 import org.octopusden.octopus.escrow.configuration.loader.EscrowConfigurationLoader;
 import org.octopusden.octopus.escrow.exceptions.EscrowConfigurationException;
 
+import static java.util.Collections.emptyMap;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.octopusden.octopus.escrow.TestConfigUtils.PRODUCT_TYPES;
 import static org.octopusden.octopus.escrow.TestConfigUtils.SUPPORTED_GROUP_IDS;
@@ -25,13 +26,21 @@ import static org.octopusden.octopus.escrow.TestConfigUtils.VERSION_NAMES;
 @DisplayName("Components registry component's validator test")
 class ComponentsRegistryValidatorTest {
 
-    private static Stream<Arguments> crsData() {
+    private static Stream<Arguments> crsInvalidData() {
         return Stream.of(
                 Arguments.of("CREG-153", Arrays.asList("groupId:artifactId patterns of module octopusstreams has intersection with octopusstreams-commons")),
                 Arguments.of("CREG-182", Arrays.asList("Archived component 'component-integration' can't be explicitly distributed. Pls set distribution->explicit=false")),
                 Arguments.of("version-prefix-intersection", Arrays.asList("Following components have no version prefix in Jira project 'PROJECT_2': component4, component5",
-                        "Following components have the same version prefix 'versionPrefix' in Jira project 'PROJECT': component1, component3"))
+                        "Following components have the same version prefix 'versionPrefix' in Jira project 'PROJECT': component1, component3")),
+                Arguments.of("test-multi-release-managers/invalid", Arrays.asList(
+                                "releaseManager is not matched '\\w+(,\\w+)*' in 'test-component-1'",
+                                "releaseManager is not matched '\\w+(,\\w+)*' in 'test-component-2'",
+                                "releaseManager is not set in 'test-component-3'"))
         );
+    }
+
+    private static Stream<String> crsValidData() {
+        return Stream.of("test-multi-release-managers/valid");
     }
 
     /**
@@ -39,23 +48,44 @@ class ComponentsRegistryValidatorTest {
      * Validation is done on loading Components Registry.
      * Verify bad configured Components Registries for expected validation errors.
      */
-    @DisplayName("Test Component Registry validation")
+    @DisplayName("Test Component Registry validation for invalid data")
     @ParameterizedTest(name = "For {0} expects {1} validation error(s)")
-    @MethodSource("crsData")
-    void testCrsValidation(
-            final String sourceComponentRegistry,
-            final Collection<String> expectedValidationErrors
-    ) throws Exception {
-        final Path aggregatorPath = Paths.get(Objects.requireNonNull(ComponentsRegistryValidatorTest.class.getResource("/" + sourceComponentRegistry + "/Aggregator.groovy")).toURI());
-        final EscrowConfigurationLoader escrowConfigurationLoader = new EscrowConfigurationLoader(
+    @MethodSource("crsInvalidData")
+    void testCrsValidationFails(final String sourceComponentRegistry,
+                                final Collection<String> expectedValidationErrors) throws Exception {
+        EscrowConfigurationLoader loader = createLoader("/" + sourceComponentRegistry + "/Aggregator.groovy");
+        assertThatThrownBy(() -> loader.loadFullConfiguration(emptyMap()))
+                .isInstanceOf(EscrowConfigurationException.class)
+                .hasMessageContainingAll(expectedValidationErrors.toArray(new String[0]));
+    }
+
+    /**
+    * Verify correctly configured Components Registries do not produce validation errors.
+    */
+    @DisplayName("Test Component Registry validation for valid data")
+    @ParameterizedTest(name = "For {0} expects no validation errors")
+    @MethodSource("crsValidData")
+    void testCrsValidationPasses(final String sourceComponentRegistry) throws Exception {
+        EscrowConfigurationLoader loader = createLoader("/" + sourceComponentRegistry + "/Aggregator.groovy");
+        assertThatCode(() -> loader.loadFullConfiguration(emptyMap())).doesNotThrowAnyException();
+    }
+
+    private static EscrowConfigurationLoader createLoader(String path) throws Exception {
+        final Path aggregatorPath = Paths.get(Objects.requireNonNull(
+                ComponentsRegistryValidatorTest.class.getResource(path)
+        ).toURI());
+        return new EscrowConfigurationLoader(
                 new ConfigLoader(
-                        ComponentRegistryInfo.createFromFileSystem(aggregatorPath.getParent().toString(),
+                        ComponentRegistryInfo.createFromFileSystem(
+                                aggregatorPath.getParent().toString(),
                                 aggregatorPath.getFileName().toString()
                         ),
                         VERSION_NAMES,
                         PRODUCT_TYPES
-                ), SUPPORTED_GROUP_IDS, SUPPORTED_SYSTEMS, VERSION_NAMES);
-        assertThatThrownBy(() -> escrowConfigurationLoader.loadFullConfiguration(Collections.emptyMap()))
-                .isInstanceOf(EscrowConfigurationException.class).hasMessageContainingAll(expectedValidationErrors.toArray(new String[0]));
+                ),
+                SUPPORTED_GROUP_IDS,
+                SUPPORTED_SYSTEMS,
+                VERSION_NAMES
+        );
     }
 }
