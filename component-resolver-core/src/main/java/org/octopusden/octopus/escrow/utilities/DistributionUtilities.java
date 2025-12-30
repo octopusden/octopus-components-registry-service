89736d9bb9d5dff31600034a0b61699d04561660
Gabriella Naomi Hutagalung <gabriellanaomi37@gmail.com>
2025-12-30 13:43:20 +0700

    add some dependency

--- component-resolver-core/src/main/java/org/octopusden/octopus/escrow/utilities/DistributionUtilities.java
+++ component-resolver-core/src/main/java/org/octopusden/octopus/escrow/utilities/DistributionUtilities.java
@@ -1,6 +1,6 @@
 package org.octopusden.octopus.escrow.utilities;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.octopusden.octopus.escrow.dto.DistributionEntity;
 import org.octopusden.octopus.escrow.dto.FileDistributionEntity;
 import org.octopusden.octopus.escrow.dto.MavenArtifactDistributionEntity;
