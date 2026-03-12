9bf3c278ae4a5d795fb2682d58ea6aeca653ca35
pgorbachev <phil.gorbachev@gmail.com>
2026-03-12 14:06:07 +0200

    Configure Java/Groovy quality checks and update common quality workflow

---
+++ config/codenarc/codenarc.groovy
@@ -0,0 +1,7 @@
+ruleset {
+    description 'CRS CodeNarc rules for Groovy validation'
+
+    ruleset('rulesets/basic.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/unnecessary.xml')
+}
