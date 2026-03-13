c7bc7f17846df35ac204645b4760316f6a25d35d
pgorbachev <phil.gorbachev@gmail.com>
2026-03-13 21:34:35 +0200

    Tune Groovy quality rules and bump oc-template

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -2,6 +2,14 @@ ruleset {
     description 'CRS CodeNarc rules for Groovy validation'
 
     ruleset('rulesets/basic.xml')
-    ruleset('rulesets/imports.xml')
-    ruleset('rulesets/unnecessary.xml')
+    ruleset('rulesets/imports.xml') {
+        MisorderedStaticImports(enabled: false)
+    }
+    ruleset('rulesets/unnecessary.xml') {
+        UnnecessaryGString(enabled: false)
+        UnnecessaryGetter(enabled: false)
+        UnnecessaryReturnKeyword(enabled: false)
+        UnnecessaryInstanceOfCheck(enabled: false)
+        UnnecessarySemicolon(enabled: false)
+    }
 }
